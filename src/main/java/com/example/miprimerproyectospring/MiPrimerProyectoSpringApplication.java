c19e57bf78706cc8ea91038a43420af68d58c945
taticd <acespedesd@gmail.com>
2021-08-23 17:24:45 -0600

    update

--- src/main/java/com/example/miprimerproyectospring/MiPrimerProyectoSpringApplication.java
+++ src/main/java/com/example/miprimerproyectospring/MiPrimerProyectoSpringApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @SpringBootApplication
 public class MiPrimerProyectoSpringApplication {
@@ -15,5 +17,15 @@ public class MiPrimerProyectoSpringApplication {
     @GetMapping(path = "api/tati")
     public String saludar() {
         return "hola tati";
+    }
+
+    @GetMapping
+    public String helloWorld(){
+        return "hello world";
+    }
+
+    @GetMapping(path = "saludos")
+    public List<String> saludos(){
+        return List.of("hola", "chao","buenas tardes","buenos dias");
     }
 }
